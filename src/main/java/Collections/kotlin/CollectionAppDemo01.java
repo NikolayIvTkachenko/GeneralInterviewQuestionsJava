04afc724d258bb1f81e4ac2b1a389a5447c3d619
NikolayIT <tkachenni@mail.ru>
2022-08-03 17:08:14 +0300

    collections commit

---
+++ src/main/java/Collections/kotlin/CollectionAppDemo01.java
@@ -0,0 +1,27 @@
+package Collections.kotlin;
+
+public class CollectionAppDemo01 {
+
+    public static void main(String[] args) {
+
+        var lists = new GenericList<String>();
+        //var iterator = lists.iterator();
+        lists.add("a");
+        lists.add("b");
+
+//        // [a, b, c]
+//        // ->
+//        while (iterator.hasNext()){
+//            var current = iterator.next();
+//            System.out.println(current);
+//        }
+
+        for (var item : lists){
+            System.out.println(item);
+        }
+
+
+
+    }
+
+}
---
+++ src/main/java/Collections/kotlin/GenericList.java
@@ -0,0 +1,58 @@
+package Collections.kotlin;
+
+import java.util.Iterator;
+
+public class GenericList<T> implements Iterable<T>{
+
+    private T[] items = (T[]) new Object[10];
+    private int count;
+
+    public GenericList() {
+    }
+
+    public T[] getItems() {
+        return items;
+    }
+
+    public void setItems(T[] items) {
+        this.items = items;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void add(T item) {items[count++] = item;}
+
+    public T get(int index) { return items[index];}
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator(this);
+    }
+
+    private class ListIterator implements Iterator<T> {
+
+        private GenericList<T> list;
+
+        private int index;
+
+        public ListIterator(GenericList<T> list){
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
+    }
+}
