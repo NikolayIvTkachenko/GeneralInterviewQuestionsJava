4813efe58022fbd5e9b3b067520abb828dbcdfee
NikolayIT <tkachenni@mail.ru>
2022-09-04 19:45:33 +0300

    update references

--- src/main/java/References/AppDemo002_References.java
+++ src/main/java/References/AppDemo002_References.java
@@ -14,12 +14,31 @@ public class AppDemo002_References {
         //Weak References //удаляется при первом GC (удобно для реализации кешей)
         WeakReference<Object> weakReference = new WeakReference<Object>(obj);
         //SoftReferences //удаляется CG когда заканчивается память
-        SoftReference<Object> softReference = new SoftReference<>(obj);
+        //SoftReference<Object> softReference = new SoftReference<>(obj);
         //PhantonRefernces //нужен, чтобы опредлить, что объект был собран сборщиком мусора
         ReferenceQueue<Object> referenceQueue = new ReferenceQueue();
         PhantomReference<Object> phantomReference = new PhantomReference<>(obj, referenceQueue);
         obj = null;
 
+        //System.out.println("softReference: " + softReference.get());
+        System.out.println("weakReference: " + weakReference.get());
+        System.out.println("phantomReference: " + phantomReference.get());
+        System.out.println("==start gc ====");
+        System.gc();
+        System.out.println("==started gc ====");
+        //System.out.println("softReference: " + softReference.get());
+        System.out.println("weakReference: " + weakReference.get());
+        System.out.println("phantomReference: " + phantomReference.get());
+
+
+
+        //Result output
+        //weakReference: java.lang.Object@119d7047
+        //phantomReference: null
+        //        ==start gc ====
+        //==started gc ====
+        //weakReference: null
+        //phantomReference: null
 
     }
 
