ca3a686e556274e71adfaa6298449f49c68bee2d
NikolayIT <tkachenni@mail.ru>
2022-09-04 19:40:45 +0300

    update stream function

--- src/main/java/References/AppDemo001_PhantomReferences.java
+++ src/main/java/References/AppDemo001_PhantomReferences.java
@@ -5,28 +5,28 @@ import java.lang.ref.ReferenceQueue;
 
 public class AppDemo001_PhantomReferences {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //PhantonRefernces //нужен, чтобы опредлить, что объект был собран сборщиком мусора
         TestObject obj = new TestObject();
         ReferenceQueue<Object> referenceQueue = new ReferenceQueue();
         PhantomReference<Object> phantomReference = new PhantomReference<>(obj, referenceQueue);
-        phantomReference.get(); //always get null
+        //phantomReference.get(); //always get null
         //referenceQueue.remove(); //будет ждать пока obj  не собирется сборщикм мусора ..прогоамма будет висеть
         obj = null;
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    referenceQueue.remove();
-                    System.out.println("reference was removed");
-                } catch (InterruptedException e) {
-                    System.out.println("Error: " + e.getMessage());
-                }
+        new Thread(() -> {
+            try {
+                referenceQueue.remove();
+                System.out.println("reference was removed");
+            } catch (InterruptedException e) {
+                System.out.println("Error: " + e.getMessage());
             }
-        }.start();
+        }).start();
+
         System.out.println("start cg");
         System.gc();
         System.out.println("gc was started");
+        Thread.sleep(3000);
+        System.out.println("End program");
     }
 
     static class TestObject {
