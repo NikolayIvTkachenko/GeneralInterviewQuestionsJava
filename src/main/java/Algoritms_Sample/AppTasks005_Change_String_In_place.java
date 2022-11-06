378d4b430e1181b506a0e098763e9856c219b55d
NikolayIT <tkachenni@mail.ru>
2022-11-07 00:56:29 +0300

    algorithms

--- src/main/java/Algoritms_Sample/AppTasks005_Change_String_In_place.java
+++ src/main/java/Algoritms_Sample/AppTasks005_Change_String_In_place.java
@@ -13,13 +13,57 @@ package Algoritms_Sample;
 */
 
 
+import java.util.Arrays;
+
 public class AppTasks005_Change_String_In_place {
 
     public static void main(String[] args) {
 
-        
+        String strInput = "abc de f";
+        strHandleRevertWords(strInput);
+
+    }
+
+    static void strHandleRevertWords(String str){
+        char[] listChars = str.toCharArray();
+        int start = 0;
+        int end = listChars.length-1;
+        System.out.println("start = " + start);
+        System.out.println("end = " + end);
+        System.out.println("before listChars = " + Arrays.toString(listChars));
+
+        while(start<end){
+            listChars = swapSymbol(start, end, listChars);
+            start +=1;
+            end -=1;
+        }
+        System.out.println("after listChars = " + Arrays.toString(listChars));
+
+        start = 0;
+        end = 0;
+        int divider = end;
+        while (start < listChars.length){
+            while((end+1 < listChars.length) && (listChars[end+1] != ' ')){
+                end += 1;
+            }
+            divider = end;
+            System.out.println("end word -> end = " + end);
+            while (start < end) {
+                listChars = swapSymbol(start, end, listChars);
+                start += 1;
+                end -= 1;
+            }
+            start = end = divider + 2;
+        }
+        System.out.println("after N2 listChars = " + Arrays.toString(listChars));
+    }
 
 
+    static char[] swapSymbol(int i1, int i2, char[] chars){
+        char cm = chars[i1];
+        chars[i1] = chars[i2];
+        chars[i2] = cm;
+        return chars;
     }
 
 }
